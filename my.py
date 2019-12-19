bc7c3f4f6a8cd32e2faa9315c541eeecd97d8ca0
root <admin_instor@ukr.net>
2019-12-19 07:27:08 +0000

    19122019

--- my.py
+++ my.py
@@ -15,10 +15,11 @@ dbname = "vtl"
 table = "vzstat"
 username = "vorobyov"
 password = "rfmd2EAA1997"
-regularpatsel = "SELECT tpsread,tpswrite,us,vzid,unxtime,mem,us_new,quota From "
+regularpatsel = "SELECT vzq,tpsread,tpswrite,us,vzid,unxtime,mem,us_new,quota From "
 update_vsu = "vzq"
 time_field = "time"
 global RRDsPath
+global vctid
 RRDsPath = '/var/www/stats/rrd/'
 
 def getCpuNum():
@@ -82,6 +83,41 @@ def SelectData(conn,fdate,ldate):
     finally :
         conn.cursor().close()
 
+def SelectDataByVCID(conn,vzid):
+    try :
+        arrayres = []
+        cursor = conn.cursor()
+        queryfordatabase = regularpatsel + table + " WHERE vzid = " + vzid
+        print("query = ",queryfordatabase)
+        cursor.execute(queryfordatabase)
+        rows = cursor.fetchall()
+        for field in rows:
+            jsonlocal={}
+            vzid = field['vzid']
+            unixtime = field['unxtime']
+            mem = field['mem']
+            us_new = field['us_new']
+            quota = field['quota']
+            tpsread = field['tpsread']
+            tpswrite = field['tpswrite']
+            cpucycles = field['us']
+	    vzq = field['vzq']
+            jsonlocal["vzid"] = vzid
+            jsonlocal["unxtime"] = unixtime
+            jsonlocal["mem"] = mem
+            jsonlocal["us_new"] = us_new
+            jsonlocal["quota"] = quota
+            jsonlocal["tpsread"] = tpsread
+            jsonlocal["tpswrite"] = tpswrite
+            jsonlocal["cpucycles"] = cpucycles
+	    jsonlocal["vzq"] = vzq
+            arrayres.append(jsonlocal)
+        return arrayres
+    except my.Error as e :
+        print("error select data function select data = ",e)
+    finally :
+        conn.cursor().close()
+
 def UpdateData(conn,sql):
     print("connection update = ",conn)
     print("sql update = ",sql)
@@ -110,29 +146,46 @@ def FormulaUpdate(Mem,CpuUsage,Space):
     except :
         print("math error operation")
 
-def writeToRRD(ctid, Mem, CpuCycles, CpuUsage, TpsRead, TpsWrite, VSU):
-    rrdPath = '%s%s.rrd' % (RRDsPath, ctid)
-    rrdParams = 'N:%s:%s:%s:%s:%s:%s' % (CpuCycles, CpuUsage, Mem, TpsWrite, TpsRead, VSU)
-    if not os.path.exists(rrdPath):
-        try:
-            rrdtool.create(str(rrdPath), '--step', '300', '--start', '0',
-                'DS:us:GAUGE:600:U:U',
-                'DS:us_new:GAUGE:600:U:U',
-                'DS:mem:GAUGE:600:U:U',
-                'DS:tpswrite:GAUGE:600:U:U',
-                'DS:tpsread:GAUGE:600:U:U',
-                'DS:sigma:GAUGE:600:U:U',
-                'RRA:AVERAGE:0.5:1:105408',
-                'RRA:MAX:0.5:1:105408')
-        except:
-            print('Error, failed to create RRD for %s.' % (ctid))
 
-    try:
-        rrdtool.update(str(rrdPath), rrdParams)
-    except:
-        print('Error, failed to update RRD for %s.' % (ctid))
+#ctid, Mem, CpuCycles, CpuUsage, TpsRead, TpsWrite, VSU
+def writeToRRD(input,ctid):
+    rrdPath = '%s%s.rrd' % (RRDsPath, ctid)
+    print("rrdPath = ",rrdPath)
+    os.remove(rrdPath)
+    if not os.path.exists(rrdPath):
+    try :
+        rrdtool.create(str(rrdPath), '--step', '60', '--start', '0',
+            'DS:us:GAUGE:600:U:U',
+            'DS:us_new:GAUGE:600:U:U',
+            'DS:mem:GAUGE:600:U:U',
+            'DS:tpswrite:GAUGE:600:U:U',
+            'DS:tpsread:GAUGE:600:U:U',
+            'DS:sigma:GAUGE:600:U:U',
+            'RRA:AVERAGE:0.5:1:105408',
+            'RRA:MAX:0.5:1:1054'
+    except :
+        print('Error, failed to create RRD for %s.' % (ctid))
+    #for
+    for item in input:
+            vzid_unparse = format(item['vzid'])           #vzid
+            unixtime = format(item['unxtime'])            #unixtime
+            mem_unparse = format(item['mem'])             #mem
+            usnew_unparse = format(item['us_new'])        #cpu usage
+            quota_unparse = format(item['quota'])         #space db
+            unparse_tpsread = format(item['tpsread'])     #tpsread
+            unparse_tpswrite = format(item['tpswrite'])   #tpswrite
+            unparse_cpucycles = format(item['cpucycles']) #cpucycles
+	    VSU = format(item['vzq'])			  #vsu
+	    rrdParams = 'N:%s:%s:%s:%s:%s:%s' % (unparse_cpucycles, usnew_unparse, mem_unparse,unparse_tpsread,unparse_tpswrite, VSU)
+            try:
+	       print("UPDATE FILE = ",rrdPath)
+               print("RRD PARAM = ",rrdParams)
+               rrdtool.update(str(rrdPath), rrdParams)
+            except:
+    	        print("error update")
 
 try:
+    vctid = []
     date_format = '%Y-%m-%d'
     if len(sys.argv) < 3:
         print("Error Not all parameter")
@@ -164,8 +217,34 @@ try:
             sqlupdate = "UPDATE " + table + " SET " + update_vsu + " = " + str(VSU) + " WHERE unxtime = " + unixtime + " AND vzid = " + vzid_unparse
             print("sqlupdate = ",sqlupdate)
             UpdateData(myconnect,sqlupdate)
+	    print("vcid = ",vctid)
+            #add container_id to array
+            if len(vctid) > 0:
+		i = 0
+		for item1 in vctid:
+		    elementislast = len(vctid) - i
+		    #esli posledniy element
+		    if elementislast == 1:
+                        if long(vctid[i]) == long(vzid_unparse):
+			    break
+			if long(elementislast) != long(vzid_unparse):
+			    vctid.append(vzid_unparse)
+			    sqlresultbyvid = SelectDataByVCID(myconnect,vzid_unparse)
+			    writeToRRD(sqlresultbyvid,ctid)
+
+		    #esli ne poskedniy element
+		    if elementislast > 1:
+                        if long(vctid[i]) == long(vzid_unparse):
+                            break
+		    i = i + 1
+            elif len(vctid) < 1:
+                vctid.append(vzid_unparse)
+                print("vctid array = ",vctid)
+		sqlresultbyvid = SelectDataByVCID(myconnect,vzid_unparse)
+		writeToRRD(sqlresultbyvid,ctid)
+
 	    #writeToRRD(ctid, Mem, CpuCycles, CpuUsage, TpsRead, TpsWrite, VSU)
-	    writeToRRD(vzid_unparse,mem_unparse,unparse_cpucycles,usnew_unparse,unparser_tpsread,unparse_tpswrite,VSU)
+	    #writeToRRD(vzid_unparse,mem_unparse,unparse_cpucycles,usnew_unparse,unparser_tpsread,unparse_tpswrite,VSU)
     except :
         print("sql error at begin")
     finally :
